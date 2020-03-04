eddba932b16aaf537ceafbb111bac9f2ff7e2715
Nam Hai Nguyen <nam.nguyen-ext@commercetools.de>
2020-03-04 12:45:46 +0100

    Add script

---
+++ scripts/check.py
@@ -0,0 +1,27 @@
+import os, subprocess
+from git import Repo
+
+repo = Repo(os.getenv('GITHUB_WORKSPACE'))
+
+diff = repo.index.diff('origin/master')
+
+ignore_dirs = [".github", "modules", "scripts"]
+
+changes = []
+for i in diff.iter_change_type('M'):
+	file_path = os.path.dirname(i.b_path)
+	for name in ignore_dirs:
+		if name in file_path:
+			break
+		changes.append(file_path.strip())
+
+# print(set(changes))
+# if not changes:
+for d in set(changes):	
+	working_dir = os.path.join(repo.working_dir, d)
+	print("-------\n", working_dir)
+	os.chdir(working_dir)
+	print('Running "terraform fmt" ...')
+	# subprocess.run(['terraform_0.12', 'fmt', '-diff=true'])
+	print('Running "terraform validate" ...')
+	subprocess.run(['terraform', 'validate'])
